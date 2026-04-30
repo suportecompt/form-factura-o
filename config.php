eaef821649b8d40e3fa2029c59203b0f9f8d5ba4
suportecompt <269455656+suportecompt@users.noreply.github.com>
2026-04-30 13:04:38 +0100

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$url1 = "http://192.168.1.147:5183/otobo/nph-genericinterface.pl/Webservice/wb1";
-$login1 = [
-    "UserLogin" => "webservice_user1",
-    "Password"  => "porto..2022"
-];
-?>
